d0a99ddd72af5efb8143f6ad04027a4b177e3420
EmmDevX <elevatedtechnology2023@gmail.com>
2026-05-26 23:18:47 +0100

    Init Commit

---
+++ api/login.js
@@ -0,0 +1,27 @@
+import pool from "../db.js";
+import bcrypt from "bcryptjs";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Only POST allowed" });
+  }
+
+  const { email, password } = req.body;
+
+  const user = await pool.query(
+    "SELECT * FROM users WHERE email=$1",
+    [email]
+  );
+
+  if (user.rows.length === 0) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const valid = await bcrypt.compare(password, user.rows[0].password);
+
+  if (!valid) {
+    return res.status(401).json({ message: "Wrong password" });
+  }
+
+  res.json({ message: "Login successful" });
+}
--- server.js
+++ server.js
@@ -19,7 +19,11 @@ pool.connect()
 
 
 app.use(cors({
-  origin: "http://127.0.0.1:5500",
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://examify-backend-vmcx.onrender.com"
+  ],
   credentials: true
 }));
 
