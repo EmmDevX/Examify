242de32b2f2d77fdc552ed20bfe6e68f0330c66c
EmmDevX <elevatedtechnology2023@gmail.com>
2026-05-27 05:50:03 +0100

    Inital Commit

---
+++ api/admin/quizzes.js
@@ -0,0 +1,13 @@
+import { pool } from "../../lib/db.js";
+
+export default async function handler(req, res) {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM quizzes ORDER BY id DESC"
+    );
+
+    res.status(200).json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
